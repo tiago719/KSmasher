9ba321b044a1c8fc22c44029bd6840275059cefe
tiago719 <tiagoaacoutinho@gmail.com>
2017-11-29 11:44:35 +0000

    Estrutura Base

---
+++ JavaApplication53/src/View/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ *
+ * @author Tiago Coutinho
+ */
+public class Constants
+{
+    static final int DIM_X_FRAME =  1000;
+    static final int DIM_Y_FRAME =  500;
+}
