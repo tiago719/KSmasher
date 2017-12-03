70c5dc48a269f9b35c3f5f069ec9c614a5108c59
EduFernandes97 <edu.cadete.fernandes@gmail.com>
2017-12-03 23:43:20 +0000

    alguns erros corrigidos

--- JavaApplication53/src/Model/Model.java
+++ JavaApplication53/src/Model/Model.java
@@ -23,18 +23,18 @@ public class Model {
     }
 
     private boolean isIF(char a[]) {
-        boolean ret;
+        boolean ret = false;
         if (a[0] == 'i' && a[1] == 'f') {
             ret = true;
         }
-        return false;
+        return ret;
     }
 
-    public void AdicionaNovoPai(PriorityQueue<ArrayList<Statement>> fp, PriorityQueue<Integer> tc, Statement add){
+    public void AdicionaNovoPai(PriorityQueue<ArrayList<Statement>> fp, PriorityQueue<Integer> tc, Statement add) {
         fp.add(add.getListaStatements());
         tc.add(add.getNumCarateresCodigoStatment() + ix);
     }
-    
+
     public void Cataloga() {
         PriorityQueue<ArrayList<Statement>> filaPais = new PriorityQueue<>(Collections.reverseOrder());
         PriorityQueue<Integer> TotalCarateres = new PriorityQueue<>(Collections.reverseOrder());
@@ -42,23 +42,27 @@ public class Model {
         boolean AspasAberto = false, PlicasAberto = false;
 
         for (; ix < Codigo.length(); ix++) {
-            if (Codigo.charAt(ix) == '"')
+            if (Codigo.charAt(ix) == '"') {
                 AspasAberto = !AspasAberto;
-            else if (Codigo.charAt(ix) == '\'')
+            } else if (Codigo.charAt(ix) == '\'') {
                 PlicasAberto = !PlicasAberto;
-            else if(!AspasAberto && !PlicasAberto){                
-                if (isIF(new char[]{Codigo.charAt(ix), Codigo.charAt(ix + 1)})) {
-                    If add = new If(Codigo.substring(ix));
-                    Pai.add(add);
-                    
-                    ix += add.getNumComecar();//para comecar a ler depois do if
+            } else if (!AspasAberto && !PlicasAberto) {
+                try {
+                    if (isIF(new char[]{Codigo.charAt(ix), Codigo.charAt(ix + 1)})) {
+                        If add = new If(Codigo.substring(ix));
+                        Pai.add(add);
 
-                    AdicionaNovoPai(filaPais, TotalCarateres, add);
+                        ix += add.getNumComecar();//para comecar a ler depois do if
+
+                        AdicionaNovoPai(filaPais, TotalCarateres, add);
+
+                    }
+                } catch (Exception e) {
 
                 }
             }
 
-            if (TotalCarateres.peek() != null && TotalCarateres.peek() == ix) {
+            if (TotalCarateres.peek() != null && TotalCarateres.peek() >= ix) {
                 TotalCarateres.remove();
                 if (filaPais.peek() != ListaStatements) {
                     Pai = filaPais.remove();
